7210ad4810f6b67f716960d2d4f8fc0f041589de
dianakaliukh <dianakaliukh09@gmail.com>
2025-11-19 14:26:16 +0200

    Create messages.py

---
+++ telegram_bot/handlers/messages.py
@@ -0,0 +1,15 @@
+from aiogram import types
+from keyboards.reply import CURRENCIES
+from handlers.utils import get_rate
+
+async def currency_handler(msg: types.Message):
+    cur = msg.text.upper()
+
+    if cur in CURRENCIES:
+        try:
+            rate = await get_rate(cur)
+            await msg.answer(f"1 {cur} = {rate:.2f} UAH (НБУ)")
+        except RuntimeError as e:
+            await msg.answer(f"Помилка отримання курсу:\n{e}")
+    else:
+        await msg.answer("Оберіть валюту з клавіатури.")
