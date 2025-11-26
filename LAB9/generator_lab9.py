1e405eae7cec155a0a0c139858e4e9d67e4e7b2c
dianakaliukh <dianakaliukh09@gmail.com>
2025-11-26 23:14:39 +0200

    Create generator_lab9.py

---
+++ LAB9/generator_lab9.py
@@ -0,0 +1,28 @@
+def pair_counter(filename="text100.txt"):
+    pairs = {"kd", "vq", "al"}
+    with open(filename, encoding="utf-8") as f:
+        for line in f:
+            words = line.strip().split()
+            count = 0
+            for i in range(len(words)):
+                w = words[i]
+                # Пари всередині слова
+                for j in range(len(w)-1):
+                    pair = w[j:j+2]
+                    if pair in pairs:
+                        count += 1
+                # Пара між словами (крім "у" в кінці і "д" на початку)
+                if i + 1 < len(words):
+                    last_char = words[i][-1]
+                    first_char = words[i+1][0]
+                    pair = last_char + first_char
+                    if pair in pairs:
+                        if not (last_char == 'у' and first_char == 'д'):
+                            count += 1
+            yield count
+
+if __name__ == "__main__":
+    total = 0
+    for c in pair_counter():
+        total += c
+    print("Загальна кількість пар:", total)
